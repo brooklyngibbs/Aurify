84c8f7fa7eac2e325729782efa438242009700a6
Brooklyn Gibbs <brooklyngibbs@Brooklyns-MacBook-Pro.local>
2024-02-17 13:12:14 -0600

    changing log in UI

--- VinylApp/Controllers/Other/SpotfiyLogInView.swift
+++ VinylApp/Controllers/Other/SpotfiyLogInView.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import MarqueeText
 
 struct AuthViewControllerWrapper: UIViewControllerRepresentable {
     @Binding var isPresented: Bool
@@ -45,13 +44,31 @@ struct SpotifyLogInView: View {
                                 .foregroundColor(Color.black)
                                 .padding(.top, 50)
                                 .padding(.leading, -180)
+                                .shadow(
+                                    color: Color.primary.opacity(0.3), /// shadow color
+                                    radius: 3, /// shadow radius
+                                    x: 0, /// x offset
+                                    y: 2 /// y offset
+                                )
                             Text("Aurify")
                                 .font(.custom("Outfit-Bold", size: 80))
                                 .foregroundColor(Color.black)
+                                .shadow(
+                                    color: Color.primary.opacity(0.3), /// shadow color
+                                    radius: 3, /// shadow radius
+                                    x: 0, /// x offset
+                                    y: 2 /// y offset
+                                )
                         }
                         .padding(.leading, 50)
                         .padding(.bottom, 60)
                     }
+
+                    TypingAnimationView()
+                        .padding(.top, -20)
+
+                    Spacer()
+                    
                     Button(action: {
                         showingAuthView = true
                     }) {
@@ -59,8 +76,11 @@ struct SpotifyLogInView: View {
                             .font(.custom("Inter-Light", size: 20))
                             .foregroundColor(.white)
                             .padding()
-                            .background(Color(AppColors.moonstoneBlue))
                             .cornerRadius(30)
+                            .background(RoundedRectangle(cornerRadius: 30)
+                                            .fill(Color(AppColors.moonstoneBlue))
+                                            .shadow(color: .gray, radius: 8, x: 0, y: 5)
+                            )
                     }
                     .sheet(isPresented: $showingAuthView) {
                         AuthViewControllerWrapper(isPresented: $showingAuthView, loginCompletion: { success in
@@ -120,5 +140,39 @@ struct SpotifyLogInView: View {
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Oops"), message: Text("Something went wrong when signing in"), dismissButton: .cancel())
         }
+        Spacer()
+    }
+}
+
+struct TypingAnimationView: View {
+    let textToType = "✨Capture Moments. Curate Playlists.✨"
+    @State private var animatedText: String = ""
+
+    var body: some View {
+        VStack {
+            Text(animatedText)
+                .font(.custom("Inter-Light", size: 18))
+                .foregroundColor(Color.gray)
+                .padding()
+
+        }
+        .onAppear {
+            animateText()
+        }
+    }
+
+    func animateText() {
+        for (index, character) in textToType.enumerated() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.05) {
+                animatedText.append(character)
+                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+            }
+        }
+    }
+}
+
+struct TypingAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        TypingAnimationView()
     }
 }
